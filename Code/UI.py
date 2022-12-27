b3e59abb9216812eddcd270fe72203edf80dad8f
Gedasikas <120640245+Gedasikas@users.noreply.github.com>
2022-12-27 13:54:02 +0200

    UI/Logic (Cordinates & Direction)

--- Code/UI.py
+++ Code/UI.py
@@ -1,17 +1,31 @@
 class Tank():
-    def __init__(self):
-        pass
+    def __init__(self, cordinateX, cordinateY, direction):
+        self.cordinateX = cordinateX
+        self.cordinateY = cordinateY
+        self.direction = direction
     def move(self, to):
         if to == "n":
+            self.direction = "Up"
+            self.cordinateY += 1
             print("Moved North")
         if to == "w":
+            self.direction = "Left"
+            self.cordinateX -= 1
             print("Moved West")
         if to == "s":
+            self.direction = "Down"
+            self.cordinateY -= 1
             print("Moved South")
         if to == "e":
+            self.direction = "Right"
+            self.cordinateX += 1
             print("Moved East")
 
-Tank001 = Tank()
+    def shoot(self):
+        print("Shot")
+
+
+Tank001 = Tank(0, 0, "Up")
 
 while True:
     userput = input("Enter tank command:")
@@ -25,7 +39,10 @@ while True:
         case "d":
             Tank001.move("e")
         case "e":
-            pass
+            Tank001.shoot()
+        case "c":
+            print((Tank001.cordinateX, Tank001.cordinateY))
+            print(Tank001.direction)
         case "":
             print ("Bye")
             break
